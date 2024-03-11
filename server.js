a12c1ddd584f7b4d02673a60153f6c56e6d7a999
manalzareen <81857933+manalzareen@users.noreply.github.com>
2024-03-11 17:38:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room",(roomId,userId,userName) =>{
+        socket.join(roomId)
+
+        socket.on("message",(message) => {
+            io.to(roomId).emit("createMessage",message,userName)
+        })
+    })
+       
+    
+});
+
+server.listen(3030);
